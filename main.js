c2781c961ee003e4cbd45b516cd7233d8db51903
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-11-21 21:42:15 +0400

    Update

---
+++ main.js
@@ -0,0 +1,15 @@
+var bar = document.getElementById("searchbar");
+var visibility = 0;
+
+function showBar() {
+    bar.style.display = "block";
+
+    // if (visibility == 0){
+    //     bar.style.display = "block";
+    //     visibility = 1
+    // }
+    // else{
+    //     bar.style.display = "none";
+    //     visibility = 0;
+    // }
+}
